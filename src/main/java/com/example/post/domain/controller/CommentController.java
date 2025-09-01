7ed5ad19dd9f19ac9c404e5e31f9bfecf38d23ab
oijwef098234 <oijwef098234@gmail.com>
2025-09-01 15:14:29 +0900

    2025.09.01

--- src/main/java/com/example/post/domain/controller/CommentController.java
+++ src/main/java/com/example/post/domain/controller/CommentController.java
@@ -2,7 +2,6 @@ package com.example.post.domain.controller;
 
 import com.example.post.domain.dto.response.PostAndCommentResponse;
 import com.example.post.domain.entity.CommentEntity;
-import com.example.post.domain.entity.PostEntity;
 import com.example.post.domain.service.CreateComment;
 import com.example.post.domain.service.DeleteComment;
 import com.example.post.domain.service.ReadComment;
@@ -22,28 +21,28 @@ public class CommentController {
     private final ReadComment readComment;
     private final DeleteComment deleteComment;
 
-    @PostMapping("/create")
-    public ResponseEntity<String> createComment(@RequestBody PostEntity postEntity, String comment){
-        createComment.createComment(postEntity, comment);
+    @PostMapping("/{id}")
+    public ResponseEntity<String> createComment(@PathVariable Long postId, @RequestBody String comment){
+        createComment.createComment(postId, comment);
 
         return ResponseEntity.ok("댓글이 생성되었습니다");
     }
 
-    @PatchMapping("/{id}") // 댓글 수정하기
+    @PatchMapping("/id}") // 댓글 수정하기
     public ResponseEntity<String> updateComment(@PathVariable Long id, @RequestBody String comment){
         updateComment.updateComment(id, comment);
 
         return ResponseEntity.ok("수정되었습니다");
     }
 
-    @GetMapping("/{id}") // 댓글 id로 불러오기
+    @GetMapping("/read-comment/{PostId}") // 댓글 id로 불러오기
     public ResponseEntity<List<CommentEntity>> readAllComment(@PathVariable Long PostId){
         List<CommentEntity> result = readComment.readAllComments(PostId);
 
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/all") // 게시글 전체 불러오기
+    @GetMapping("/all/{postId}") // 게시글 전체 불러오기
     public ResponseEntity<PostAndCommentResponse> readPostAndComments(Long postId){
         PostAndCommentResponse result = readComment.readPostAndComments(postId);
 
@@ -57,5 +56,3 @@ public class CommentController {
         return ResponseEntity.ok(result);
     }
 }
-// 지금 controller에서 댓글을 매핑된 메서드안에 집어넣지도 못하고 있고 또한 지피티가 추천해준 방식을 보니 PostEnitity를 받는것이 아닌 postId를 받는거기 떄문에
-// entity도 고치고 다른것들을 다 고쳐야할거 같음 ㅇㅇ
--- src/main/java/com/example/post/domain/service/CreateComment.java
+++ src/main/java/com/example/post/domain/service/CreateComment.java
@@ -3,6 +3,7 @@ package com.example.post.domain.service;
 import com.example.post.domain.entity.CommentEntity;
 import com.example.post.domain.entity.PostEntity;
 import com.example.post.domain.repository.CommentRepository;
+import com.example.post.domain.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +11,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CreateComment {
     private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
 
-    public void createComment(PostEntity post, String comment) {
+    public void createComment(Long postId, String comment) {
+        PostEntity post = postRepository.findById(postId).get();
+
         CommentEntity commentEntity = new CommentEntity();
 
-        commentEntity.setContent(comment);
         commentEntity.setPostEntity(post);
+        commentEntity.setContent(comment);
 
         commentRepository.save(commentEntity);
     }
