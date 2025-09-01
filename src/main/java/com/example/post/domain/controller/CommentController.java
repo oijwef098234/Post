e1b7e2851d735a086b242c909c3cfae3d642dddc
oijwef098234 <oijwef098234@gmail.com>
2025-09-01 15:42:09 +0900

    2025.09.01

--- src/main/java/com/example/post/domain/controller/CommentController.java
+++ src/main/java/com/example/post/domain/controller/CommentController.java
@@ -22,29 +22,29 @@ public class CommentController {
     private final DeleteComment deleteComment;
 
     @PostMapping("/{id}")
-    public ResponseEntity<String> createComment(@PathVariable Long postId, @RequestBody String comment){
-        createComment.createComment(postId, comment);
+    public ResponseEntity<String> createComment(@PathVariable Long id, @RequestBody String comment){
+        createComment.createComment(id, comment);
 
         return ResponseEntity.ok("댓글이 생성되었습니다");
     }
 
-    @PatchMapping("/id}") // 댓글 수정하기
+    @PatchMapping("/{id}") // 댓글 수정하기
     public ResponseEntity<String> updateComment(@PathVariable Long id, @RequestBody String comment){
         updateComment.updateComment(id, comment);
 
         return ResponseEntity.ok("수정되었습니다");
     }
 
-    @GetMapping("/read-comment/{PostId}") // 댓글 id로 불러오기
-    public ResponseEntity<List<CommentEntity>> readAllComment(@PathVariable Long PostId){
-        List<CommentEntity> result = readComment.readAllComments(PostId);
+    @GetMapping("/read-comment/{id}") // 댓글 id로 불러오기
+    public ResponseEntity<List<CommentEntity>> readAllComment(@PathVariable Long id){
+        List<CommentEntity> result = readComment.readAllComments(id);
 
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/all/{postId}") // 게시글 전체 불러오기
-    public ResponseEntity<PostAndCommentResponse> readPostAndComments(Long postId){
-        PostAndCommentResponse result = readComment.readPostAndComments(postId);
+    @GetMapping("/all/{id}") // 게시글 전체 불러오기
+    public ResponseEntity<PostAndCommentResponse> readPostAndComments(@PathVariable Long id){
+        PostAndCommentResponse result = readComment.readPostAndComments(id);
 
         return ResponseEntity.ok(result);
     }
